85e4cd8acb87eb4daba08b99c14c17d945ff3bd8
importstring <sbergeron@torontoprepschool.com>
2026-01-04 20:41:43 -0500

    Add metadata and main function calls in main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+__author__ = "Simon ;)"
+__date__ = "2026-01-04"
+__status__ = "100% - Overconfident Andy"
+__course__ = "ICS3U"
+__teacher__ = "Mr. G"
+__credits__ = "My dog + infinite debugging"
+__mood__ = "Ship sunk, still smiling"
+__difficulty__ = "Hard (for the computer)"
+__high_score_holder__ = "Also Simon"
+__bug_count__ = "0 (that I know of)"
+__lines_of_code__ = "Too many to count"
+__testing_strategy__ = "If it works on my machine, it ships"
+__future_self_note__ = "Don't write file metadata past 8pm"
+
+from src.battleship import main as callIT
+from src.rubric import run_extras_demo as methodsforMrG
+
+if __name__ == "__main__":
+    # "Modularity"
+    callIT()
+    methodsforMrG()
